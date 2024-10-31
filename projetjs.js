3dca87bc1ce493f30eb9d329920858953b52c3be
Hélène Darte <hl.dar@dwwm.evx>
2024-10-31 16:53:48 +0000

    js exercice debut

---
+++ projetjs.js
@@ -0,0 +1,16 @@
+let compteur = 2;
+function maFonction() {
+    let myVar = 456;
+    console.log("myVar:" + myVar);
+}
+if (compteur > 1) {
+    let myVar2 = "Wazaa!";
+    console.log("myVar2:" + myVar2);
+}
+console.log("myVar: " + myVar);
+
+console.log("myVar2:" + myVar2);
+
+
+
+
